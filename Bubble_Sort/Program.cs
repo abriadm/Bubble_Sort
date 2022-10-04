b31a9d4ee15628592d37c7daa187e08506f1038c
nrchy <nrchy@ABRIADM>
2022-10-04 07:32:56 +0700

    Add project files.

---
+++ Bubble_Sort/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Bubble_Sort
+{
+    class Program
+    { 
+        private int[] a = new int[20];
+
+        private int n;
+
+        public void read()
+        {
+            while (true)
+            {
+                Console.Write("Masukan banyaknya elemen pada Array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 20 elemen.\n");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Masukan elemen array");
+            Console.WriteLine("--------------------------");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                a[i] = Int32.Parse(s1);
+            }
+            
+        }
+        public void display()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Element array yang telah tersusun");
+            Console.WriteLine();
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(a[j]);
+            }
+            Console.WriteLine("");
+        }
+        public void BubbleSortArray()
+        {
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 0; j < n - i; j++)
+                {
+                    if (a[j] > a[j + 1])
+                    {
+                        int temp;
+                        temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        { 
+            Program mylist = new Program();
+            mylist.read();
+            mylist.BubbleSortArray();
+            mylist.display();
+            Console.WriteLine("\n\nTekan apa saja untuk keluar.");
+            Console.Read();
+        }
+
+    }
+}
